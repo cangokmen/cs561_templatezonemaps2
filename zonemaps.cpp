dbae8e0fe19965de5fa643cf34afb9c550f4d245
Can Gokmen <cgokmen@scc-wl2.scc.bu.edu>
2025-01-25 14:02:59 -0500

    Started implementing the zonemap

--- zonemaps.cpp
+++ zonemaps.cpp
@@ -1,16 +1,51 @@
 #include "zonemaps.h"
+#include <algorithm>
 
-template<typename T>
+
+// this is correct
+template <typename T>
 zonemap<T>::zonemap(std::vector<T> _elements, uint _num_elements_per_zone)
+    : elements(_elements), num_elements_per_zone(_num_elements_per_zone) 
 {
     // constructor 
     // Your code starts here ...
+    
+    num_zones = (elements.size() + num_elements_per_zone - 1) / num_elements_per_zone; // correct
+
+    build();
 }
 
-template<typename T>
-size_t zonemap<T>::query(T key)
+
+
+template <typename T>
+void zonemap<T>::build()
 {
-    // Your code starts here ...
-    return 0;
+    //sort_elements();
+
+    for (size_t i = 0; i < elements.size(); i += num_elements_per_zone) 
+    {
+        zone<T> z;
+
+        size_t end = std::min(i + num_elements_per_zone, elements.size());
+        z.elements.assign(elements.begin() + i, elements.begin() + end);
+
+        z.min = z.elements.front(); // since not sorted, incorrect!!
+        z.max = z.elements.back(); // since not sorted, incorrect!!
+        z.size = z.elements.size(); // correct
+
+        zones.push_back(z);
+    }
 }
+
+
+
+
+
+template <typename T>
+void zonemap<T>::sort_elements()
+{
+    std::sort(elements.begin(), elements.end());
+}
+
+
 
